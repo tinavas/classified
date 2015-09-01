61beb90a4662578440699b1b30d82bbba5ce2532
Taha <muhammadtahakhan222@gmail.com>
2015-09-01 12:10:40 +0500

    Add Main layout configuration

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'layout'=>'webmain',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
